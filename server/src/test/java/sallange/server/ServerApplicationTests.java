a945d0c39a5b0e596b6dbf0f53e35dcf2e5fe209
gitchan <eunkeeeee@gmail.com>
2023-10-22 18:10:29 +0900

    feat: initial setting

---
+++ server/src/test/java/sallange/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sallange.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
