1bda50afa991dd26992cf1eb96192a6fd48a35e4
gitchan <eunkeeeee@gmail.com>
2023-10-31 10:20:06 +0900

    feat: health check api 추가

---
+++ server/src/main/java/sallange/server/api/HealthCheckController.java
@@ -0,0 +1,16 @@
+package sallange.server.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> heathCheck() {
+        return ResponseEntity.ok("up");
+    }
+}
