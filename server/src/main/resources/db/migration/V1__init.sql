6f03b80aceff19dd989146f403ca3425fbcbb730
gitchan <eunkeeeee@gmail.com>
2023-10-31 17:34:46 +0900

    refactor: 데이터베이스 초기화 테이블 스크립트 필드 순서 변경

--- server/src/main/resources/db/migration/V1__init.sql
+++ server/src/main/resources/db/migration/V1__init.sql
@@ -1,31 +1,31 @@
 create table cycle
 (
-    created_at datetime(6),
     id         bigint not null auto_increment,
-    updated_at datetime(6),
     status     enum ('AVAILABLE','BROKEN','RENT'),
+    created_at datetime(6),
+    updated_at datetime(6),
     primary key (id)
 ) engine=InnoDB;
 
 create table rent_history
 (
-    created_at datetime(6),
-    cycle_id   bigint,
     id         bigint not null auto_increment,
-    updated_at datetime(6),
     user_id    bigint,
+    cycle_id   bigint,
     type       enum ('RENT','RETURN'),
+    created_at datetime(6),
+    updated_at datetime(6),
     primary key (id)
 ) engine=InnoDB;
 
 create table users
 (
-    left_rent_count integer,
-    created_at      datetime(6),
     id              bigint not null auto_increment,
-    oauth_id        bigint,
-    updated_at      datetime(6),
     name            varchar(255),
     oauth_provider  enum ('GOOGLE','KAKAO'),
+    oauth_id        bigint,
+    left_rent_count integer,
+    created_at      datetime(6),
+    updated_at      datetime(6),
     primary key (id)
 ) engine=InnoDB;
