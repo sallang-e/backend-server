66e38b1ce361eb5326b9cb53af2661546d824897
gitchan <eunkeeeee@gmail.com>
2023-10-31 13:29:03 +0900

    feat: JWT Token 관련 클래스 추가

---
+++ server/src/main/java/sallange/server/auth/application/client/AuthTokensGenerator.java
@@ -0,0 +1,40 @@
+package sallange.server.auth.application.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import sallange.server.auth.api.response.AuthTokensResponse;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokensGenerator {
+
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24;       // 24시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthTokensResponse generate(Long memberId) {
+        long now = (new Date()).getTime();
+        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+        String subject = memberId.toString();
+
+        String accessToken = jwtTokenProvider.generate(subject, accessTokenExpiredAt);
+
+        String refreshToken = jwtTokenProvider.generate(subject, refreshTokenExpiredAt);
+
+        return AuthTokensResponse.of(accessToken, refreshToken, BEARER_TYPE, ACCESS_TOKEN_EXPIRE_TIME / 1000L);
+    }
+
+    public boolean isValidToken(String accessToken) {
+        return jwtTokenProvider.isValidToken(accessToken);
+    }
+
+    public Long extractMemberId(String accessToken) {
+        return Long.valueOf(jwtTokenProvider.extractSubject(accessToken));
+    }
+}
