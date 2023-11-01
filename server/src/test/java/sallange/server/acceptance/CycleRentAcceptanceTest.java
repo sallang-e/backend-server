10e09c48ae692b29634e76cbbb8af4853b68c091
gitchan <eunkeeeee@gmail.com>
2023-11-01 15:45:25 +0900

    test: 테스트 코드 추가

--- server/src/test/java/sallange/server/acceptance/CycleRentAcceptanceTest.java
+++ server/src/test/java/sallange/server/acceptance/CycleRentAcceptanceTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static sallange.server.acceptance.step.CycleCreateStep.살랑이_생성_요청하고_아이디_반환;
 import static sallange.server.acceptance.step.CycleRentStep.살랑이_대여_요청;
+import static sallange.server.acceptance.step.CycleReturnStep.살랑이_반납_요청;
 import static sallange.server.acceptance.step.UsersJoinStep.회원_가입_요청하고_액세스_토큰_반환;
 
 class CycleRentAcceptanceTest extends AcceptanceTest {
@@ -46,6 +47,24 @@ class CycleRentAcceptanceTest extends AcceptanceTest {
     void 살랑이_사용_가능_회수가_없는_유저가_살랑이를_대여하면_에러_코드_3을_반환한다() {
         final String accessToken = 회원_가입_요청하고_액세스_토큰_반환(new UserJoinRequest("깃짱", "KAKAO", 123L, 0));
         final Long cycleId = 살랑이_생성_요청하고_아이디_반환();
+
+        final ExtractableResponse<Response> response = 살랑이_대여_요청(accessToken, RentRequest.from(cycleId));
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(BAD_REQUEST.value()),
+                () -> assertThat(response.jsonPath().getInt("errorCode")).isEqualTo(3)
+        );
+    }
+
+    @Test
+    void 회원가입_후에_3번_빌리면_대여가_안된다() {
+        final String accessToken = 회원_가입_요청하고_액세스_토큰_반환(new UserJoinRequest("깃짱", "KAKAO", 123L, 2));
+        final Long cycleId = 살랑이_생성_요청하고_아이디_반환();
+
+        살랑이_대여_요청(accessToken, RentRequest.from(cycleId));
+        살랑이_반납_요청(accessToken);
+        살랑이_대여_요청(accessToken, RentRequest.from(cycleId));
+        살랑이_반납_요청(accessToken);
 
         final ExtractableResponse<Response> response = 살랑이_대여_요청(accessToken, RentRequest.from(cycleId));
 
