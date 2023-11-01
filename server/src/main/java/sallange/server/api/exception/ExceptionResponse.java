2dc9b71ed4171c8edf1f819e1fb08c7ea3a7fdff
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-11-01 17:28:50 +0900

    feat: exception handler 추가 (#11)

---
+++ server/src/main/java/sallange/server/api/exception/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package sallange.server.api.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse {
+
+    private String exceptionMessage;
+}
--- server/src/main/java/sallange/server/api/exception/GlobalExceptionHandler.java
+++ server/src/main/java/sallange/server/api/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package sallange.server.api.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import sallange.server.exception.RentException;
+import sallange.server.exception.UnAuthorizationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +15,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .badRequest()
                 .body(new RentExceptionResponse(e.getErrorCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(UnAuthorizationException.class)
+    public ResponseEntity<ExceptionResponse> handleUnAuthorizationException(final UnAuthorizationException e) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ExceptionResponse(e.getExceptionMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
+        return ResponseEntity
+                .internalServerError()
+                .body(new ExceptionResponse(e.getMessage()));
     }
 }
