b68295d0a5d53b02e96addf79dd130ef4d693fec
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-11-01 17:32:47 +0900

    fix: interceptor 세팅 변경 (#12)

--- server/src/main/java/sallange/server/config/WebMvcConfig.java
+++ server/src/main/java/sallange/server/config/WebMvcConfig.java
@@ -3,8 +3,10 @@ package sallange.server.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sallange.server.config.argumentresolver.UserAuthArgumentResolver;
+import sallange.server.config.interceptor.UserAuthInterceptor;
 
 import java.util.List;
 
@@ -12,13 +14,15 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final UserAuthInterceptor userAuthInterceptor;
     private final UserAuthArgumentResolver userAuthArgumentResolver;
 
-//    @Override
-//    public void addInterceptors(final InterceptorRegistry registry) {
-//        registry.addInterceptor(userAuthInterceptor)
-//                .excludePathPatterns("/admin/**", "/test/**", "/api/login/**");
-//    }
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(userAuthInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login/**");
+    }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
