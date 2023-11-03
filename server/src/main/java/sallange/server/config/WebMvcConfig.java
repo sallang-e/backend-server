70ea3fac1da4a3b1dc46296dc97b40ebc4898975
gitchan <eunkeeeee@gmail.com>
2023-11-04 02:30:47 +0900

    feat: CORS 설정 아주 널널하게

--- server/src/main/java/sallange/server/config/WebMvcConfig.java
+++ server/src/main/java/sallange/server/config/WebMvcConfig.java
@@ -36,9 +36,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://sallang-e.or.kr"
-                );
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+        ;
     }
 }
