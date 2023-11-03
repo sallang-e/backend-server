8e5e806bda8a30cae0433ad952d219531deaba25
깃짱 <107979804+eunkeeee@users.noreply.github.com>
2023-11-03 12:59:36 +0900

    feat: 로깅 필터 추가 (#15)
    * feat: 로깅 필터 추가
    
    * feat: security 업데이트

---
+++ server/src/main/java/sallange/server/config/RequestLoggingFilter.java
@@ -0,0 +1,38 @@
+package sallange.server.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    private final ThreadPoolTaskExecutor executor;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("thread-id : {}, thread-name: {}, pool-size: {}", Thread.currentThread().getId(), Thread.currentThread().getName(), executor.getPoolSize());
+        MDC.put("requestId", UUID.randomUUID().toString());
+        MDC.put("requestMethod", request.getMethod());
+        MDC.put("requestUrl", request.getRequestURI());
+
+        String authorization = request.getHeader("authorization");
+        MDC.put("authorization", authorization);
+        if (authorization == null) {
+            MDC.put("authorization", "null");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
